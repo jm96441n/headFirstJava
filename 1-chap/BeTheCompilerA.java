9e1f620e657d51e08c7b09b091d2d420f6f20192
John Maguire <john@jmaguire.tech>
2018-08-07 19:40:35 -0400

    be the compiler exercises done

---
+++ 1-chap/BeTheCompilerA.java
@@ -0,0 +1,11 @@
+// will compile fine
+class BeTheCompilerA {
+  public static void main(String[] args) {
+    int x = 1;
+    while ( x <  10 ) {
+      if ( x < 3 ) {
+        System.out.println("big X");
+      }
+    }
+  }
+}
