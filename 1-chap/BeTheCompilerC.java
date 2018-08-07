3f1bda9f1ed9b96d5bdbadbb2937ada3ad8a3bea
John Maguire <jmaguire@mdsol.com>
2018-08-07 19:40:35 -0400

    be the compiler exercises done

---
+++ 1-chap/BeTheCompilerC.java
@@ -0,0 +1,10 @@
+// won't compile, missing main method
+class BeTheCompilerC {
+  int x = 5;
+  while ( x > 1 ) {
+    x = x - 1;
+    if ( x < 3 ) {
+      System.out.println("small x");
+    }
+  }
+}
