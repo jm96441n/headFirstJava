ccd7de912090f9c2ba322108706f853e39bb0fbc
John Maguire <john@jmaguire.tech>
2018-08-11 14:23:17 -0400

    added code with blanks to be filled in

---
+++ 2-chap/PoolPuzzle/Echo.java
@@ -0,0 +1,6 @@
+class // replace with code {
+  int // replace with code = 0;
+  void // replace with code {
+    System.out.println("helloooo...");
+  }
+}
---
+++ 2-chap/PoolPuzzle/EchoTestDrive.java
@@ -0,0 +1,26 @@
+// expected output
+// helloooo...
+// helloooo...
+// helloooo...
+// helloooo...
+// 10
+
+public class EchoTestDrive {
+  public static void main(String[] args) {
+    Echo e1 = new Echo();
+    // replace with code
+    int x = 0;
+    while ( // replace with code ) {
+      el.hello();
+      // replace with code
+      if ( // replace with code ) {
+        e2.count = e2.count + 1;
+      }
+      if ( // replace with code ) {
+        e2.count = e2.count + e1.count;
+      }
+      x = x + 1;
+    }
+    System.out.println(e2.count);
+  }
+}
