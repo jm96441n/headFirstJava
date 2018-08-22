296f7e57d6085a518d0ae7cc5d476a08c9ad568e
John Maguire <jmaguire@mdsol.com>
2018-08-21 22:25:10 -0400

    added skeleton for pool puzzle

---
+++ 4-chap/PoolPuzzle/Puzzle4.java
@@ -0,0 +1,31 @@
+public class Puzzle4 {
+  public static void main(String[] args) {
+    ______________________________________
+    int y = 1;
+    int x = 0;
+    int result = 0;
+    while ( x < 6 ) {
+      ___________________________________
+      ___________________________________
+      y = y * 10;
+      __________________________________
+    }
+    x = 6;
+    while ( x < 0 ) {
+      __________________________________
+      result = result + __________________
+    }
+    System.out.println("result " + result);
+  }
+}
+
+class _________ {
+  int ivar;
+  _________   ______________ doStuff(int _______) {
+    if ( ivar > 100 ) {
+      return __________
+    } else {
+      return ____________
+    }
+  }
+}
