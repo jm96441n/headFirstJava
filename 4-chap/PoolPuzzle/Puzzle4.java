beddcd6a7871425a4b94c215b1ff9b6f1c651607
John Maguire <john@jmaguire.tech>
2018-08-21 22:38:13 -0400

    solved pool puzzle

--- 4-chap/PoolPuzzle/Puzzle4.java
+++ 4-chap/PoolPuzzle/Puzzle4.java
@@ -1,31 +1,31 @@
 public class Puzzle4 {
   public static void main(String[] args) {
-    ______________________________________
+    Puzzle4b[] obs = new Puzzle4b[6];
     int y = 1;
     int x = 0;
     int result = 0;
     while ( x < 6 ) {
-      ___________________________________
-      ___________________________________
+      obs[x] = new Puzzle4b();
+      obs[x].ivar = y;
       y = y * 10;
-      __________________________________
+      x = x + 1;
     }
     x = 6;
-    while ( x < 0 ) {
-      __________________________________
-      result = result + __________________
+    while ( x > 0 ) {
+      x = x - 1;
+      result = result + obs[x].doStuff(x);
     }
     System.out.println("result " + result);
   }
 }
 
-class _________ {
+class Puzzle4b {
   int ivar;
-  _________   ______________ doStuff(int _______) {
+  public int doStuff(int factor) {
     if ( ivar > 100 ) {
-      return __________
+      return ivar * factor;
     } else {
-      return ____________
+      return ivar * (2 + factor);
     }
   }
 }
